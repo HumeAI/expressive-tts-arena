701fd0fa1e22a0b427aa7540a9c05fee3229939a
zach <zachary.greathouse@hume.ai>
2025-02-06 14:41:15 -0800

    Add voting results data structure to house voting results

--- src/types.py
+++ src/types.py
@@ -1,17 +1,19 @@
 """
 types.py
 
-This module defines custom types for the application to enforce the structure
-of the options map used in the user interface. This ensures that each option
-has a consistent structure including both the provider and the associated voice.
+This module defines custom types for the application.
 """
 
 # Standard Library Imports
-from typing import Literal, Dict
+from typing import Dict, Literal, TypedDict
 
 
 TTSProviderName = Literal["Hume AI", "ElevenLabs"]
 """TTSProviderName represents the allowed provider names for TTS services."""
+
+
+ComparisonType = Literal["Hume AI - Hume AI", "Hume AI - ElevenLabs"]
+"""Comparison type denoting which providers are compared."""
 
 
 OptionKey = Literal["Option A", "Option B"]
@@ -19,5 +21,19 @@ OptionKey = Literal["Option A", "Option B"]
 
 
 OptionMap = Dict[OptionKey, TTSProviderName]
-"""OptionMap defines the structure of the options mapping, where each key is an OptionKey
-and the value is an OptionDetails dictionary."""
+"""OptionMap defines the structure of the options mapping, where each key is an OptionKey and the value is a TTS provider."""
+
+
+class VotingResults(TypedDict):
+    """Voting results data structure representing values we want to persist to the votes DB"""
+
+    comparison_type: str
+    winning_provider: TTSProviderName
+    winning_option: OptionKey
+    option_a_provider: TTSProviderName
+    option_b_provider: TTSProviderName
+    option_a_generation_id: str
+    option_b_generation_id: str
+    voice_description: str
+    text: str
+    is_custom_text: bool
